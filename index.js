8eaaef555100117152062dd77e0341ac529f15f1
varad-n <hvi924926@gmail.com>
2025-07-29 01:45:45 +0530

    in progress

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express')
+import { mongoDBURL } from './config.js';
+import express, { response } from "express";
+import mongoose from "mongoose";
 const app = express()
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-});
+
 
 app.get('/', (req, res) => {
     res.send("Hello from Node API");
 });
+
+
+mongoose.connect(mongoDBURL)
+.then(() => {
+    console.log("Connected to the Database!");
+    app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+});
+})
+.catch(() => {
+    console.log("Connection Failed!");
+})
