d9f7467f6a0aecedda00a4edc9af54111f2b6ecc
varad-n <hvi924926@gmail.com>
2025-07-31 23:56:59 +0530

    in progress

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ app.get('/', (req, res) => {
     res.send("Hello from Node API");
 });
 
-app.post('/api/products', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error){
+        res.status(500).json({message: error.message});
+    }
 });
 
 
