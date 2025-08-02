dba9e9d1dd9ab8112d6ded17887994e867ec5817
varad-n <hvi924926@gmail.com>
2025-08-03 00:27:15 +0530

    delete API added

--- index.js
+++ index.js
@@ -11,22 +11,22 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/products', async (req, res) => {
-    try{
+    try {
         const products = await Product.find({});
         res.status(200).json(products);
-    } catch(error){
-        res.status(500).json({message: error.message});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 
 
 app.get('/api/products/:id', async (req, res) => {
-    try{
+    try {
         const { id } = req.params;
         const product = await Product.findById(id);
         res.status(200).json(product);
-    } catch(error){
-        res.status(500).json({message: error.message})
+    } catch (error) {
+        res.status(500).json({ message: error.message })
     }
 })
 
@@ -34,38 +34,54 @@ app.post('/api/products', async (req, res) => {
     try {
         const product = await Product.create(req.body);
         res.status(200).json(product);
-    } catch (error){
-        res.status(500).json({message: error.message});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 
 
-app.put('/api/products/:id', async (req, res) =>{
-    try{
-        const {id} = req.params;
+app.put('/api/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
 
         const product = await Product.findByIdAndUpdate(id, req.body);
 
-        if(!product) {
-            return res.status(404).json({message: "Product not Found!"});
+        if (!product) {
+            return res.status(404).json({ message: "Product not Found!" });
         }
 
         const updatedProduct = await Product.findById(id);
         res.status(200).json(updatedProduct);
 
-    }catch(error){
-        res.status(500).json({message: error.message})
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+
+app.delete('/api/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found!" });
+        }
+
+        res.status(200).json({ message: "Product Deleted Successfully" });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 })
 
 
 mongoose.connect(mongoDBURL)
-.then(() => {
-    console.log("Connected to the Database!");
-    app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-});
-})
-.catch(() => {
-    console.log("Connection Failed!");
-})
+    .then(() => {
+        console.log("Connected to the Database!");
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000')
+        });
+    })
+    .catch(() => {
+        console.log("Connection Failed!");
+    })
