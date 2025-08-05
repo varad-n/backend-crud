55887555f24327999a899e81947c3567806da104
varad-n <hvi924926@gmail.com>
2025-08-06 02:19:37 +0530

    kinda completed

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { mongoDBURL } from './config.js';
-import express, { response } from "express";
+import express from "express";
 import mongoose from "mongoose";
-const app = express()
+const app = express();
 import Product from './models/product.model.js';
+import productRoute from './routes/product.route.js';
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.use("/api/products", productRoute);
@@ -14,70 +15,6 @@ app.use("/api/products", productRoute);
 app.get('/', (req, res) => {
     res.send("Hello from Node API");
 });
-
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-app.get('/api/products/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
-
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-app.put('/api/products/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-
-        const product = await Product.findByIdAndUpdate(id, req.body);
-
-        if (!product) {
-            return res.status(404).json({ message: "Product not Found!" });
-        }
-
-        const updatedProduct = await Product.findById(id);
-        res.status(200).json(updatedProduct);
-
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
-
-
-app.delete('/api/products/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByIdAndDelete(id);
-        if (!product) {
-            return res.status(404).json({ message: "Product not found!" });
-        }
-
-        res.status(200).json({ message: "Product Deleted Successfully" });
-
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
 
 
 mongoose.connect(mongoDBURL)
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,2 +1,16 @@
 import express from "express";
 const router = express.Router();
+import Product from "../models/product.model.js";
+import { getProducts, getProduct, createProduct, updateProduct, deleteProduct } from "../controllers/product.controller.js";
+
+router.get("/", getProducts);
+
+router.get("/:id", getProduct);
+
+router.post("/", createProduct);
+
+router.put("/:id", updateProduct);
+
+router.put("/:id", deleteProduct)
+
+export default router;
